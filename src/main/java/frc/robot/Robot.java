ebbee2c0bfdd67121ea18c1e4e24ecd80359633b
Chris B <115883974+Darkwolf7898@users.noreply.github.com>
2024-05-03 22:14:40 -0400

    changed PIDs

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -90,16 +90,12 @@ URCL.start();
 // /*END */
 
     var talonFXConfigs = new TalonFXConfiguration();
-
-
-    // set slot 0 gains
     var slot0Configs = talonFXConfigs.Slot0;
-    slot0Configs.kS = 0.25; // Add 0.25 V output to overcome static friction
+    slot0Configs.kS = 0.05; // Add 0.05 V output to overcome static friction
     slot0Configs.kV = 0.12; // A velocity target of 1 rps results in 0.12 V output
-    slot0Configs.kA = 0.01; // An acceleration of 1 rps/s requires 0.01 V output
-    slot0Configs.kP = 4.8; // A position error of 2.5 rotations results in 12 V output
+    slot0Configs.kP = 0.11; // An error of 1 rps results in 0.11 V output
     slot0Configs.kI = 0; // no output for integrated error
-    slot0Configs.kD = 0.1; // A velocity error of 1 rps results in 0.1 V output
+    slot0Configs.kD = 0; // no output for error derivative
 
     talonFXConfigs.TorqueCurrent.PeakForwardTorqueCurrent = 40;
     talonFXConfigs.TorqueCurrent.PeakReverseTorqueCurrent = -40;
